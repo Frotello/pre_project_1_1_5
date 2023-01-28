b344dbbc80bb62f59b40b55ebd77d5399c8ce370
Vlad Panin <vlad.frotello@gmail.com>
2023-01-28 20:20:41 +0300

    First version of the solution

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,28 @@
 package jm.task.core.jdbc.util;
 
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/testdb";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root1234";
+    private static Connection connection = null;
+
+    public static Connection getConnection(){
+
+        try  {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            if (!connection.isClosed()) {
+                System.out.println("Соединение с БД установлено");
+            }
+            return connection;
+        } catch (SQLException e) {
+            System.out.println("Соединение с БД не установлено");
+        }
+            return connection;
+
+    }
 }
