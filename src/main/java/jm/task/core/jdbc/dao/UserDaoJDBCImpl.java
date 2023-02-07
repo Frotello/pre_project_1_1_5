1ac1b23737ea75ef6261af6b376ac439d1b2f980
Vlad Panin <vlad.frotello@gmail.com>
2023-02-07 15:38:10 +0300

    work with Hibernate

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -13,78 +13,34 @@ public class UserDaoJDBCImpl implements UserDao {
 
     }
 
-
+    @Override
     public void createUsersTable() {
-        try (Connection someConnection = Util.getConnection();
-             Statement statement = someConnection.createStatement()) {
-            statement.executeUpdate("CREATE TABLE IF NOT EXISTS users " +
-                    "(id BIGINT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255), last_name VARCHAR(255), age INT)");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
 
     }
 
+    @Override
     public void dropUsersTable() {
-        try (Connection someConnection = Util.getConnection();
-             Statement statement = someConnection.createStatement()) {
-            statement.executeUpdate("DROP TABLE IF EXISTS users");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+
     }
 
+    @Override
     public void saveUser(String name, String lastName, byte age) {
-        try (Connection someConnection = Util.getConnection();
-             PreparedStatement pstm = someConnection.prepareStatement("INSERT INTO users (name, last_name, age) VALUES (?, ?, ?)")
-        ) {
-            pstm.setString(1, name);
-            pstm.setString(2, lastName);
-            pstm.setByte(3, age);
-            pstm.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+
     }
 
+    @Override
     public void removeUserById(long id) {
-        try (Connection someConnection = Util.getConnection();
-             PreparedStatement pstm = someConnection.prepareStatement("DELETE FROM users WHERE id = ?")
-        ) {
-            pstm.setLong(1, id);
-            pstm.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+
     }
 
+    @Override
     public List<User> getAllUsers() {
-        List<User> users = new ArrayList<>();
-
-        try (Connection someConnection = Util.getConnection();
-             ResultSet resultSet = someConnection.createStatement().executeQuery("SELECT * FROM users")
-        ) {
-            while (resultSet.next()) {
-                User user = new User(resultSet.getString("name"),
-                        resultSet.getString("last_name"), resultSet.getByte("age"));
-                user.setId(resultSet.getLong("id"));
-                users.add(user);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        return users;
+        return null;
     }
 
+    @Override
     public void cleanUsersTable() {
-        try (Connection someConnection = Util.getConnection();
-             Statement statement = someConnection.createStatement()
-        ) {
-            statement.executeUpdate("TRUNCATE TABLE users");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+
     }
 }
 
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,29 +1,8 @@
 package jm.task.core.jdbc.util;
 
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import org.hibernate.SessionFactory;
 
 public class Util {
-    private static final String URL = "jdbc:mysql://localhost:3306/testdb";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "root1234";
-    private static Connection connection = null;
-
-    public static Connection getConnection(){
-
-        try  {
-            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-            if (!connection.isClosed()) {
-                System.out.println("Соединение с БД установлено");
-            }
-
-
-        } catch (SQLException e) {
-            System.out.println("Соединение с БД не установлено");
-        }
-        return connection;
-
-    }
+    private static  SessionFactory concreteSessionFactory;
 }
